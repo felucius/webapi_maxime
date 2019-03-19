214981ffef77016da864509b9578f0cfde354a95
M. de Lange <maximedelange1991@hotmail.com>
2019-03-19 17:07:01 +0100

    Jenkins CI build test 10

--- WebAPI/WebApplication1/WebApplication1/Controllers/ValuesController.cs
+++ WebAPI/WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "jenkins test", "jenkins test 10" };
+            return new string[] { "jenkins test", "jenkins test 11" };
         }
 
         // GET api/values/5
